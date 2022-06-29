e385350de365a6e06051208ca151292bbfbea955
DaviGSR <davireis0403@gmail.com>
2022-06-29 12:02:41 -0300

    revisao

---
+++ js/escrevendo-aula1.js
@@ -0,0 +1,7 @@
+var buttom = document. querySelector("button");
+
+buttom.onclick = function() {
+var nome = prompt ("Qual é o comando para iniciar o git?");
+if ((nome = "git init")) {
+ alert ("O" + nome+ "serve para iniciar o git.parabéns voce acertou!")}
+};
